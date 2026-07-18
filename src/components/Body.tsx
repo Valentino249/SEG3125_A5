3c5d697e02e4fdaba2098bcc3fd666d58ca62655
Valentino249 <valentino.vinod@gmail.com>
2026-07-18 15:08:34 -0400

    line chart works

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,15 +1,32 @@
+import { ResponsiveContainer, LineChart, XAxis, YAxis, Tooltip, Legend, Line } from "recharts";
 
 interface HeroProps {
   onNavigate: (page: string) => void;
 }
 
-function Hero({ onNavigate }: HeroProps) {
+const data = [
+    { name: "John", value: 400},
+    { name: "Joe", value: 500},
+    { name: "Jill", value: 100},
+    { name: "June", value: 600}
+]
+
+function Body({ onNavigate }: HeroProps) {
     return (
         <div>
-            <div className="relative h-screen ">
-                <div className="absolute inset-0 flex flex-col items-center justify-center">
-                    <h1 className="text-5xl lg:text-9xl text-black bg-white rounded px-5 font-bold">BOTTOM TEXT</h1>
+            <div className="relative h-screen">
+                <ResponsiveContainer width={600} height={300}>
+                        <LineChart data={data}>
+                            <XAxis dataKey="name"/>
+                            <YAxis />
+                            <Tooltip/>
+                            <Legend/>
+                            <Line type="monotone" dataKey="value" stroke="#8884d8"/>
+                        </LineChart>
+                    </ResponsiveContainer>
                     
+                <div className="absolute inset-0 flex flex-col items-center justify-center">
+                    {/* <h1 className="text-5xl lg:text-9xl text-black bg-white rounded px-5 font-bold">BOTTOM TEXT</h1> */}
                     
                 </div>
             </div>
@@ -18,4 +35,4 @@ function Hero({ onNavigate }: HeroProps) {
     )
 }
 
-export default Hero
+export default Body
