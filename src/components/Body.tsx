d7425e57df667be9721e2cba1d12d31fcb8115a8
Valentino249 <valentino.vinod@gmail.com>
2026-07-18 17:27:55 -0400

    second graph has settings

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -40,20 +40,49 @@ function Body({ onNavigate }: HeroProps) {
         { month: "May", value: 31.14}
     ]
 
-const data_2 = [
+const data_jan = [
     { cut: "Beef stewing", value: 22.79},
     { cut: "Beef striploin", value: 35.27},
     { cut: "Beef top sirloin", value: 28.95},
     { cut: "Beef rib", value: 39.33},
 ]
 
+const data_feb = [
+    { cut: "Beef stewing", value: 23.90},
+    { cut: "Beef striploin", value: 35.56},
+    { cut: "Beef top sirloin", value: 24.87},
+    { cut: "Beef rib", value: 37.70},
+]
+
+const data_mar = [
+    { cut: "Beef stewing", value: 21.61},
+    { cut: "Beef striploin", value: 40.09},
+    { cut: "Beef top sirloin", value: 29.22},
+    { cut: "Beef rib", value: 35.69},
+]
+
+const data_april = [
+    { cut: "Beef stewing", value: 22.51},
+    { cut: "Beef striploin", value: 42.42},
+    { cut: "Beef top sirloin", value: 28.94},
+    { cut: "Beef rib", value: 30.56},
+]
+
+const data_may = [
+    { cut: "Beef stewing", value: 23.86},
+    { cut: "Beef striploin", value: 35.30},
+    { cut: "Beef top sirloin", value: 30.60},
+    { cut: "Beef rib", value: 31.14},
+]
+
     const [dataSelection, setDataSelection] = useState(data_stewing);
     const [dataName, setDataName] = useState("Stewing")
 
-     const handleSubmit = (e:any) => {
-        console.log(e)
-      
+    const [data2Selection, setData2Selection] = useState(data_jan);
+    const [dataMonth, setDataMonth] = useState("January")
 
+    const handleSubmit = (e:any) => {
+        console.log(e)
       switch (e) {
         case "stewing":
             console.log("1")
@@ -76,13 +105,43 @@ const data_2 = [
             setDataName("Rib")
             break;
       }
-      
+   
   }
+    const handleSubmit2 = (e:any) => {
+        console.log(e)
+        switch (e) {
+        case "jan":
+            console.log("1")
+            setData2Selection(data_jan)
+            setDataMonth("January")
+            break;
+        case "feb":
+            console.log("2")
+            setData2Selection(data_feb)
+            setDataMonth("February")
+            break;
+        case "march":
+            console.log("3")
+            setData2Selection(data_mar)
+            setDataMonth("March")
+            break;
+        case "april":
+            console.log("4")
+            setData2Selection(data_april)
+            setDataMonth("April")
+            break;
+        case "may":
+            console.log("5")
+            setData2Selection(data_may)
+            setDataMonth("May")
+            break;
+      }
+    }
     return (
         <div>
-            <div className="relative h-screen">
+            <div className="relative h-full">
 
-                <div className="mt-5 mb-5 grid grid-cols-2 gap-5">
+                <div className="mt-5 mb-5 grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 gap-5">
                     <div className="flex flex-col">
                         <p className="text-xl mb-4">Price of Beef {dataName} Cuts (per kg) First Half of 2026</p>
                         <ResponsiveContainer width={600} height={600}>
@@ -95,7 +154,7 @@ const data_2 = [
                             </LineChart>
                         </ResponsiveContainer>
                         
-                            <select name="options" id="options" onChange={e => handleSubmit(e.target.value)}>
+                        <select className="text-center" name="options" id="options" onChange={e => handleSubmit(e.target.value)}>
                             <option value="stewing">Stewing</option>
                             <option value="striploin">Striploin</option>
                             <option value="top sirloin">Top Sirloin</option>
@@ -106,12 +165,12 @@ const data_2 = [
                     </div>
                     
                     <div className="flex flex-col">
-                        <p className="text-xl mb-4">Price of Various Beef Cuts (per kg) January 2026</p>
+                        <p className="text-xl mb-4">Price of Various Beef Cuts (per kg) {dataMonth} 2026</p>
                         <ResponsiveContainer width={600} height={600}>
                             <BarChart
                                 style={{ width: '100%', maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.618 }}
                                 responsive
-                                data={data_2}
+                                data={data2Selection}
                                 margin={{
                                     top: 5,
                                     right: 0,
@@ -129,6 +188,14 @@ const data_2 = [
                                 
                             </BarChart>
                         </ResponsiveContainer>
+
+                        <select className="text-center" name="options2" id="options" onChange={e => handleSubmit2(e.target.value)}>
+                            <option value="jan">January</option>
+                            <option value="feb">February</option>
+                            <option value="march">March</option>
+                            <option value="april">April</option>
+                            <option value="may">May</option>
+                        </select>
                     </div>
                 
                 </div>
